24c8390c6571ae0f7ab43959de8f83e3134a92bf
gaciuki2402 <delbrivigrace@gmail.com>
2022-01-28 18:39:30 +0300

    initial commit

--- pro.py
+++ pro.py
@@ -2,12 +2,12 @@ import random
 concent=input("Welcome!Do you wanna play, Rock, Paper,Scissors..?(Yes/No):")
 concent=concent.lower()
 if concent=="yes" or concent=="y":
-    score=0
-    tie=0
+    score = 0
+    tie = 0
     while True:
         possible_actions = ["rock", "paper", "scissors"]
         computer_guess = random.choice(possible_actions)
-        print(f"compGuess:{computer_guess}")
+
         attempt=input("Come on Grace, Enter your try,(choose between rock,paper,scissors):")
         attempt.lower()
         if attempt=="rock":
@@ -20,22 +20,24 @@ if concent=="yes" or concent=="y":
             elif computer_guess=="Scissors":
                 print("Hurray! Rock smashes scissors")
                 score+=1
-                print("Score {}".format(score))
-            else:
-                print("Press Enter to continue")
+                print("Score: {}".format(score))
+
+            print(f"compGuess:{computer_guess}")
         elif attempt=="paper":
             if computer_guess=="rock":
                 print("Hurray!--->paper covers rock. You won!")
                 score+=1
                 print("Score {}".format(score))
+                print(f"Score {score}")
             elif computer_guess=="paper":
                 print("It\'s a tie!")
                 tie+=1
                 print("Tie: {}".format(tie))
             elif computer_guess=="scissors":
                 print("Oops!--->scissors cut paper. You lose!")
-            else:
-                print("---."*56)
+
+            print(f"compGuess:{computer_guess}")
+
         elif attempt=="scissors":
             if computer_guess=="rock":
                 print("Oops!---> rock crushes scissors.You lose!")
@@ -46,16 +48,11 @@ if concent=="yes" or concent=="y":
             elif computer_guess=="scissors":
                 tie+=1
                 print("It\'s a tie.")
-            else:
-                print("+"*30)
+            print(f"compGuess:{computer_guess}")
         else:
             print("Invalid Entry, please Try again!!!")
 elif concent=="no" or concent=="n":
     print("Exited Successfully.")
-
-else:
-    print("Invalid Entry!!!")
-    exit()
 
 
 
